c7074b1aacba0c9ce8d0a87680cb8df4277da33a
gabiler <lerda.gabriel@gmail.com>
2022-07-26 07:24:40 -0300

    calculador de prestamos

--- javascript/index.js
+++ javascript/index.js
@@ -7,8 +7,45 @@
 //     alert(nombreCompleto)
 // }
 
-let apellido = prompt('Adivina el apellido de Luna')
+/*let apellido = prompt('Adivina el apellido de Luna').toUpperCase
 while (apellido != 'Rico') {
     apellido = prompt('No adivinaste, ingresa otro') ; 
 }
-alert('Muy bien!! adivinaste, su apellido es Rico')
+alert('Muy bien!! adivinaste, su apellido es Rico')*/
+
+
+//=================================Desafio entregable 1=========================================
+function financiacion(num1, num2, num3, num4) {  
+    return num1 * (num2 + num3 * num4)
+}
+
+function cuotas(montoFinal, cuotas) {
+    return montoFinal / cuotas
+}
+
+const tasa = 0.045
+const num = 1
+let monto;
+let cantCuotas;
+let interes;
+let cuota;
+let salida;
+
+function calculador() {
+    monto = Number(prompt('Indique el monto que desea financiar'))
+    if (monto === 0) return
+    cantCuotas = Number(prompt('Ingrese la cantidad de cuotas en la cual desea financiar'))
+    interes = financiacion(monto, num, tasa, cantCuotas)
+    cuota = cuotas(interes,cantCuotas)
+    alert('Usted va a devolver un total de ' + interes)
+    alert('En ' + cantCuotas + ' cuotas de ' + cuota )
+    // alert('Gracias por usar nuestro calculador')
+}//Si descomentas este alert y comentas el while el calculador funciona con el boton html
+
+while(salida != 'NO') {
+    calculador()
+    salida = prompt('Desea hacer otro cálculo? SI o NO').toUpperCase()
+}
+alert('Gracias por usar nuestro calculador')
+
+
